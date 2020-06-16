0b966b96f01093659011ca2f03dbef762582e85a
Youcef Soukkou <youcefsoko66@gmail.com>
2020-06-16 11:08:11 +0100

    Added DNA to RNA Transcription and DNA Reverse Complement Routines

--- DNAToolkit/DNAToolkit.py
+++ DNAToolkit/DNAToolkit.py
@@ -1,8 +1,10 @@
 #DNA Toolkit File
 import random 
+# import collections
 
 # For Re-Use Purposes
 NUCLEOTIDES = ["A","C","T","G"]
+NUCLEOTIDESREVERSECOMPLEMENT = {"A":"T", "T":"A", "C":"G", "G":"C"}
 
 # Checks the Validity of a DNA Sequence
 def isValidDNA(DNA):
@@ -10,17 +12,15 @@ def isValidDNA(DNA):
 	for nucleotide in upperDNA:
 		if nucleotide not in NUCLEOTIDES:
 			return False
-	return upperDNA
+	return True
 
 # Generates a Valid Random DNA Sequence With a Given Length 
 def genDNA(length):
 	randomDNASequence = "".join([random.choice(NUCLEOTIDES) for nucleotide in range(length)])
 	return randomDNASequence
 
-# Counts the Number of Nucleotides in a Given DNA Sequence
-def nucCount(DNA):
-	if(isValidDNA(DNA) == False):
-		return "Invalid DNA Sequence!"
+# Counts the Total Number of All Nucleotides in a Given DNA Sequence
+def nucAllCount(DNA):
 	numNucleotides = 0
 	for nucleotide in DNA : 
 		numNucleotides+=1 
@@ -28,9 +28,17 @@ def nucCount(DNA):
 
 # Counts The Frequency of Each Nucleotide in a Given DNA Sequence	
 def nucFrequencyCount(DNA):
-	if(isValidDNA(DNA) == False):
-		return "Invalid DNA Sequence!"
 	NucleotideFrequencies = {"A" : 0 , "C" : 0 , "T" : 0 , "G" : 0 }
 	for nucleotide in DNA : 
 		NucleotideFrequencies[nucleotide] += 1 
 	return NucleotideFrequencies	
+	# return dict(collections.Counter(seq))
+
+# DNA Transcription to RNA
+def DNA2RNA(DNA) :
+	return DNA.replace("T","U")
+
+# Generates a Reverse Complement of a DNA Strand
+def DNAReverseComplement(DNA):
+	return ''.join([NUCLEOTIDESREVERSECOMPLEMENT[nuc] for nuc in DNA])[::-1]
+	
