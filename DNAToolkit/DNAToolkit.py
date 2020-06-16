03bfb39bea0cfeb32932804d4a323e5608ccd4ee
Youcef Soukkou <youcefsoko66@gmail.com>
2020-06-16 16:19:12 +0100

    Some Bug Fixes + Formatting

--- DNAToolkit/DNAToolkit.py
+++ DNAToolkit/DNAToolkit.py
@@ -13,16 +13,12 @@ def isValidDNA(DNA):
     return True
 
 # Generates a Valid Random DNA Sequence With a Given Length
-
-
 def genDNA(length):
     randomDNASequence = "".join([random.choice(NUCLEOTIDES)
                                  for nucleotide in range(length)])
     return randomDNASequence
 
 # Counts the Total Number of All Nucleotides in a Given DNA Sequence
-
-
 def nucAllCount(DNA):
     numNucleotides = 0
     for nucleotide in DNA:
@@ -30,8 +26,6 @@ def nucAllCount(DNA):
     return numNucleotides
 
 # Counts The Frequency of Each Nucleotide in a Given DNA Sequence
-
-
 def nucFrequencyCount(DNA):
     NucleotideFrequencies = {"A": 0, "C": 0, "T": 0, "G": 0}
     for nucleotide in DNA:
@@ -40,20 +34,14 @@ def nucFrequencyCount(DNA):
     # return dict(collections.Counter(seq))
 
 # DNA Transcription to RNA
-
-
 def DNA2RNA(DNA):
     return DNA.replace("T", "U")
 
 # Generates a Reverse Complement of a DNA Strand
-
-
 def DNAReverseComplement(DNA):
     return ''.join([NUCLEOTIDESREVERSECOMPLEMENT[nuc] for nuc in DNA])[::-1]
 
 # Calculates CG Content % , use optional arguments 'start' and 'end' to select a specefic sub-sequence
-
-
 def GCContent(DNA, **kwargs):
     subSeqStart = kwargs.get('start', None)
     subSeqEnd = kwargs.get('end', None)
--- DNAToolkit/main.py
+++ DNAToolkit/main.py
@@ -1,2 +1,4 @@
 
 from DNAToolkit import *
+dna = "CAGCA"
+print(GCContent(dna, start=1, end=3))
